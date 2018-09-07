eb977c2dccf7d2c5ee506994973ef593afe6bc64
Thiru PS <srikarasu@gmail.com>
2018-09-08 00:58:59 +1000

    Update script.sql

--- ddl/script.sql
+++ ddl/script.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS db_table_source1;CREATE TABLE `db_table_source1` (
+CREATE TABLE `db_table_source1` (
  `PurchaseID` bigint(20) NOT NULL,
   `CustomerID` bigint(20) NOT NULL,
   `ProductID` int(20) NOT NULL,
