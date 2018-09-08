54f96867c6cf023a8331508a3ee06b15a5850ba5
Thiru PS <srikarasu@gmail.com>
2018-09-08 14:21:37 +1000

    Update script.sql

--- ddl/script.sql
+++ ddl/script.sql
@@ -1,9 +1,9 @@
-drop table  IF EXISTS `db_table_source1`;
+drop table  IF EXISTS `db_table_source1` /n
 CREATE TABLE `db_table_source1` (
   `PurchaseID` bigint(20) NOT NULL,
   `CustomerID` bigint(20) NOT NULL,
   `ProductID` int(20) NOT NULL,
   `DOP` datetime DEFAULT NULL,
   `Amount` float DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
