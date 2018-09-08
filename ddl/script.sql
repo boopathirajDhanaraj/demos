25bc828412855cc295102af6e2501c5b717d04dd
Thiru PS <srikarasu@gmail.com>
2018-09-08 14:11:19 +1000

    Update script.sql

--- ddl/script.sql
+++ ddl/script.sql
@@ -1,5 +1,4 @@
-drop table  IF EXISTS `db_table_source1`; \n
-commit;
+drop table  IF EXISTS `db_table_source1`;
 CREATE TABLE `db_table_source1` (
   `PurchaseID` bigint(20) NOT NULL,
   `CustomerID` bigint(20) NOT NULL,
@@ -7,4 +6,4 @@ CREATE TABLE `db_table_source1` (
   `DOP` datetime DEFAULT NULL,
   `Amount` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-commit;
+
