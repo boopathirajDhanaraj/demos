a498d464a142fca4b65f26f935bf36df0a50788b
Thiru PS <srikarasu@gmail.com>
2018-09-08 12:26:29 +1000

    Update script.sql

--- ddl/script.sql
+++ ddl/script.sql
@@ -1,10 +1,13 @@
 SET NAMES 'utf8';
-DELETE TABLE db_table_source1;
-CREATE TABLE db_table_source1 (
-  PurchaseID bigint(20) NOT NULL,
-  CustomerID bigint(20) NOT NULL,
-  ProductID int(20) NOT NULL,
-  DOP datetime DEFAULT NULL,
-  Amount float DEFAULT NULL
+drop table  IF EXISTS `db_table_source1`;
+commit;
+CREATE TABLE `db_table_source1` (
+  `PurchaseID` bigint(20) NOT NULL,
+  `CustomerID` bigint(20) NOT NULL,
+  `ProductID` int(20) NOT NULL,
+  `DOP` datetime DEFAULT NULL,
+  `Amount` float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+commit;
+
 
