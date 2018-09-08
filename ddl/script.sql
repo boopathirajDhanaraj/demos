fd65ddf364505408d25d3888a46ea1c321524c6e
Thiru PS <srikarasu@gmail.com>
2018-09-08 12:32:22 +1000

    Update script.sql

--- ddl/script.sql
+++ ddl/script.sql
@@ -1,13 +1,11 @@
-SET NAMES "ascii";
-drop table  IF EXISTS "db_table_source1";
+SET NAMES 'ascii';
+drop table  IF EXISTS 'db_table_source1';
 commit;
-CREATE TABLE `db_table_source1` (
-  `PurchaseID` bigint(20) NOT NULL,
-  `CustomerID` bigint(20) NOT NULL,
-  `ProductID` int(20) NOT NULL,
-  `DOP` datetime DEFAULT NULL,
-  `Amount` float DEFAULT NULL
+CREATE TABLE 'db_table_source1' (
+  'PurchaseID' bigint(20) NOT NULL,
+  'CustomerID' bigint(20) NOT NULL,
+  'ProductID' int(20) NOT NULL,
+  'DOP' datetime DEFAULT NULL,
+  'Amount' float DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 commit;
-
-
